460215f9ba6626504c16bde379b5544e0abf172c
VivekCitrusbug <vivek1.citrusbug@gmail.com>
2025-01-08 18:25:52 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+## Github configuration
+GITHUB_CLIENT_SECRET="5103393d1b9519a21e895de3e38ce1e051ba669e"
+GITHUB_AUTHORIZATION_BASE_URL = "https://github.com/login/oauth/authorize"
+GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
+GITHUB_API_URL = "https://api.github.com/user"
+GITHUB_REDIRECT_URI = "https://5365-182-70-122-97.ngrok-free.app" 
+GITHUB_CLIENT_ID = "Iv23lirqlvDErqbtVJKO"
